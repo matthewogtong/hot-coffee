db0fd4336eb9aade2780605c8b28902fe9caee53
matthewogtong <ogtongm@gmail.com>
2022-01-21 12:09:20 -0500

    Initial commit

---
+++ HotCoffee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotCoffee
+//
+//  Created by Matthew Ogtong on 1/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
