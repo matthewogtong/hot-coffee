6ada6762d69b7ae388017071f2a0142f239e52ef
matthewogtong <ogtongm@gmail.com>
2022-01-21 15:26:28 -0500

    Create VCs for Orders Table View and Add new order

---
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddOrderViewController.swift
+//  HotCoffee
+//
+//  Created by Matthew Ogtong on 1/21/22.
+//
+
+import Foundation
+import UIKit
+
+class AddOrderViewController: UIViewController {
+    
+    
+    
+}
---
+++ HotCoffee/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OrdersTableViewController.swift
+//  HotCoffee
+//
+//  Created by Matthew Ogtong on 1/21/22.
+//
+
+import Foundation
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+    
+    
+    
+}
