6166325318cc97acf8141b74d01ce4db34f4e60f
matthewogtong <ogtongm@gmail.com>
2022-01-22 18:45:30 -0500

    Create enum for CoffeeSize

--- HotCoffee/Models/Order.swift
+++ HotCoffee/Models/Order.swift
@@ -7,11 +7,19 @@
 
 import Foundation
 
+enum CoffeeSize: String, Codable {
+    
+    case small
+    case medium
+    case large
+    
+}
+
 struct Order: Codable {
     
     let name: String
     let coffeeName: String
     let total: Float
-    let size: String
+    let size: CoffeeSize
     
 }
