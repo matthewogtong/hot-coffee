b60fd2764313d59e2b913f0f996e189611a80596
matthewogtong <ogtongm@gmail.com>
2022-01-25 10:32:26 -0500

    Implement numberOfSections method

--- HotCoffee/Controllers/OrdersTableViewController.swift
+++ HotCoffee/Controllers/OrdersTableViewController.swift
@@ -40,4 +40,10 @@ class OrdersTableViewController: UITableViewController {
         
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    
 }
