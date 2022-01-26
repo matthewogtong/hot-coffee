1af713baad25e28912fab856028cc9d40a2e5126
matthewogtong <ogtongm@gmail.com>
2022-01-26 12:17:21 -0500

    Set delegate and dataSource to AddOrderVC and implement numberofRowsInSection and cellForRowAt to display coffee selections in table view

--- HotCoffee/Controllers/AddOrderViewController.swift
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -12,13 +12,19 @@ class AddOrderViewController: UIViewController {
     
     private var vm = AddCoffeeOrderViewModel()
     
+    @IBOutlet weak var coffeSelectionTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.coffeSelectionTableView.delegate = self
+        self.coffeSelectionTableView.dataSource = self
         
     }
     
 }
+
+//MARK: - UITableViewDelegate
 
 extension AddOrderViewController: UITableViewDelegate {
     
@@ -26,6 +32,21 @@ extension AddOrderViewController: UITableViewDelegate {
     
 }
 
+//MARK: - UITableViewDataSource
+
 extension AddOrderViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vm.types.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = coffeSelectionTableView.dequeueReusableCell(withIdentifier: "CoffeeSelectionTableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = vm.types[indexPath.row]
+        
+        return cell
+    }
     
 }
