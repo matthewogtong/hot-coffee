2d277bb1021a3a7745ed51677a42fc12db405a29
matthewogtong <ogtongm@gmail.com>
2022-01-28 12:46:10 -0500

    Add selectedType and selectedSize properties to AddCoffeeOrderVM

--- HotCoffee/View Models/AddCoffeeOrderViewModel.swift
+++ HotCoffee/View Models/AddCoffeeOrderViewModel.swift
@@ -11,6 +11,10 @@ struct AddCoffeeOrderViewModel {
     
     var name: String?
     
+    var selectedType: String?
+    
+    var selectedSize: String?
+    
     var types: [String] {
         return CoffeeName.allCases.map { $0.rawValue }
     }
